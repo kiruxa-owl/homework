dacc4fe9df500138018aebd7105a84bb49081bd6
kiruxa-owl <kirillka_kuznecov@mail.ru>
2024-09-09 12:40:26 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+number_of_complited_hw=(12)
+number_of_hours_spent=(90)
+course_name='Python'
+time_for_one_task=(number_of_hours_spent / number_of_complited_hw )
+print(course_name,'total tasks',number_of_complited_hw,'hours spent',number_of_hours_spent,'average execution time',time_for_one_task)
