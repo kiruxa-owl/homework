e4cd41e4f9970ebd02086656b5e1518600251a6f
kiruxa-owl <kirillka_kuznecov@mail.ru>
2024-09-10 19:31:32 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+# immutable_var= 1, 2, 3 , 4
+# print(immutable_var)
+# immutable_var[0]= 100
+# print(immutable_var)
+mutable_list= [1, 2, 3, 4]
+print(mutable_list)
+mutable_list[0]= 20
+print(mutable_list)
