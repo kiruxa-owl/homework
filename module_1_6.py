555209bd99c22e9a20c8197482cb59e20f3abe0e
kiruxa-owl <kirillka_kuznecov@mail.ru>
2024-09-12 03:21:32 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,17 @@
+# my_dict= {'Kirill': 22, 'Danil': 26}
+# print(my_dict)
+# print(my_dict['Kirill'])
+# print(my_dict.get('Max'))
+# my_dict['Andrey']= 30
+# my_dict['Katya']= 25
+# print(my_dict)
+# a=my_dict.pop('Kirill')
+# print(a)
+# print(my_dict)
+my_set={1, 2, 2, 1, 4, 4, 3, 3, 6,}
+print(my_set)
+my_set.add(5)
+my_set.add(7)
+my_set.discard(2)
+print(my_set)
+
