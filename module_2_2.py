4ddf0d67aff6e13fd608c853b29acbcfd71872ed
kiruxa-owl <kirillka_kuznecov@mail.ru>
2024-09-28 18:45:29 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+
+if first == second == third:
+    print('Вывод: 3')
+elif first == second or first == third or second == third:
+    print('Вывод: 2')
+else:
+    print('Вывод: 0')
