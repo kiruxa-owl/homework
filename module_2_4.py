32ef20c300dc4a35be6948ec16b11dcfdcd2d615
kiruxa-owl <kirillka_kuznecov@mail.ru>
2024-09-28 18:57:38 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+primes = []
+not_primes = []
+
+for number in numbers:
+    if number == 1:
+        continue
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(number)
+    else:
+        not_primes.append(number)
+
+print("Primes:", primes)
+print("Not Primes:", not_primes)
